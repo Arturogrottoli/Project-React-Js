d6427ff8d5b5cdd629b71af142157ffa7b5789a8
Arturo Grottoli <arturogrottoli@gmail.com>
2025-09-30 20:51:04 -0300

    Clase 5b

--- Clase05/src/App.jsx
+++ Clase05/src/App.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import AsyncAwait from './componentes/AsyncAwait/AsyncAwait'
-import JsonPlaceHolder from './componentes/JsonPlaceHolder/JsonPlaceHolder'
-import Nesflic from './componentes/Nesflic/Nesflic'
-import Form from './componentes/Form/Form'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Computadoras from "./componentes/Computadoras/Computadoras"
+import Sillas from "./componentes/Sillas/Sillas"
+import Celulares from "./componentes/Celulares/Celulares"
+import Menu from "./componentes/Menu/Menu"
+import Error from "./componentes/Error/Error"
 
 const App = () => {
 
 
   return (
     <div>
-      {/* <AsyncAwait/> */}
-      {/* <JsonPlaceHolder/> */}
-      {/* <Nesflic/> */}
-      <Form/>
+      <BrowserRouter>
+        <Menu/>
+        <Routes>
+          <Route path="/computadoras" element={<Computadoras/>}/>
+          <Route path="/sillas/:id" element={<Sillas/>}/>
+          <Route path="/celulares" element={<Celulares/>}/>
+          {/* <Route path="*" element={<Error/>}/> */}
+        </Routes>
+      </BrowserRouter>
+
+
+
+
+
+
+
+
+      {/* <h1>Etiquetas de Enlaces:</h1>
+      <h2>Enlaces Absolutos y realtivos</h2>
+      <a href="https://www.infobae.com" target="_blank">Infobae</a>
+      <a href="">Contacto</a> */}
     </div>
   )
 }
