ed7f4cb3246adca76e6fb71fb2b11d3498b0b97c
Arturo Grottoli <arturogrottoli@gmail.com>
2026-03-02 20:00:54 -0300

    clase 3b

--- Clase03/src/App.jsx
+++ Clase03/src/App.jsx
@@ -1,5 +1,22 @@
+import React from 'react'
+import Articulos from './componentes/Articulos/Articulos'
+import ComponenteX from './componentes/ComponenteX/ComponenteX'
+import Contador from './componentes/Contador/Contador'
+import Categorias from './componentes/Categorias/Categorias'
+import Ref from './componentes/Ref/Ref'
 import NavBar from './componentes/NavBar/NavBar'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
+
+
+// Características de los componentes (repaso rápido):
+// - Renderizan un único árbol/elemento raíz
+// - Pueden recibir props: datos primitivos, objetos/arreglos y funciones
+// - Pueden anidar contenido mediante children
+
+// En esta clase mostramos:
+// - Props y children (Articulos + ComponenteX)
+// - Estado y efectos (Contador, Categorias)
+// - useRef para valores mutables no reactivos (Ref)
 
 
 const App = () => {
@@ -12,7 +29,7 @@ const App = () => {
          * <Articulos img="https://picsum.photos/200/300" titulo="Alimentos para gatos"/>
          * <Articulos img="https://picsum.photos/200/400" titulo="Vacunas para gatos"/>
          * <Articulos img="https://picsum.photos/200/200" titulo="Juguetes para gatos"/>
-         * <Articulos img="https://picsum.photos/200/200" titulo="Casas para gatos"> 
+         * <Articulos img="https://picsum.photos/200/200" titulo="Casas para gatos">
          *   <p>Contenido como children (texto, etiquetas, componentes).</p>
          *   <strong>Tiempo de lectura: 3 minutos</strong>
          *   <ComponenteX/>
@@ -26,8 +43,11 @@ const App = () => {
         {/** <Categorias/> */}
 
         {/* Ejemplo de useRef: valor mutable que no re-renderiza */}
-        <Ref/>
+        {/** <Ref/> */}
 
+        <NavBar/>
+        <ItemListContainer greeting="Bienvenido a la tienda! Estos son nuestros productos."/>
+    <Ref/>
     </div>
   )
 }
